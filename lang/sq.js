ad94aa431f47028e7d78fa1e8641fa140102f4bb
Wiktor Walc <w.walc@cksource.com>
2013-04-22 16:29:28 +0200

    Updated language files, added Albanian translation.

--- lang/sq.js
+++ lang/sq.js
@@ -2,6 +2,6 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'preview', 'fr-ca', {
-	preview: 'Prévisualiser'
+CKEDITOR.plugins.setLang( 'preview', 'sq', {
+	preview: 'Parashiko'
 });
